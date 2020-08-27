e77e02816df77d9e692dad84e15ec69cefe08a4b
Demian Brovkin <bro.d3mk@gmail.com>
2020-08-27 17:37:03 +0300

    added parameterized test and home task description

--- src/test/java/tests/homework_7/TestsForJenkins.java
+++ src/test/java/tests/homework_7/TestsForJenkins.java
@@ -4,9 +4,17 @@ import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+/**
+ * 1. Зарегистрироваться в Jenkins https://jenkins.autotests.cloud/
+ * 2. Написать простейший автотест и добиться чтобы можно было запустить тесты из терминала командой типа "gradle some_tests"
+ * 3. Сделать джобу в дженкинсе:
+ * - Указать в настройках джобы ссылку на свой гитхаб-проект
+ * - Указать команду для запуска.
+ * 4. Добавьте в код автотестов возможность передавать параметр из терминала
+ * 5. И добавьте в вашу джобу в jenkins возможнось передавать параметр в запуск тестов
+ */
 
 public class TestsForJenkins {
     static Logger logger = LoggerFactory.getLogger(TestsForJenkins.class);
