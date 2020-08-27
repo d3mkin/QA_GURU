a2c2e6d08c65e0bfabf80c21652b6a0e75fc275b
Demian Brovkin <bro.d3mk@gmail.com>
2020-08-27 17:33:18 +0300

    added parameterized test

--- src/test/java/tests/homework_7/TestsForJenkins.java
+++ src/test/java/tests/homework_7/TestsForJenkins.java
@@ -2,17 +2,30 @@ package tests.homework_7;
 
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 public class TestsForJenkins {
-    @Tag("stringTest")
+    static Logger logger = LoggerFactory.getLogger(TestsForJenkins.class);
+
+    @Tag("simpleStringTest")
     @Test
-    public void StringTest() {
+    public void stringTest() {
         String a = "Hello, Jenkins!";
         String b = "Hello, Jenkins!";
 
         assertEquals(a,b, "Hello, Jenkins!");
+    }
+
+    @Tag("parameterizedStringTest")
+    @Test
+    public void parameterizedStringTest() {
+        String paramText = System.getProperty("paramText", "Hello, Jenkins!");
+        logger.info("\nparamText = " + paramText);
+
+        assertEquals("Hello, Jenkins!", paramText, "text = " + paramText);
     }
 }
